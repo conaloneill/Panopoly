fa888f97862974e185942a6a67a6063505683f06
Conal O'Neill <conal.oneill95@gmail.com>
2018-06-12 17:42:52 +0100

    updated server class

--- server/src/websocket/Server.java
+++ server/src/websocket/Server.java
@@ -4,7 +4,21 @@ import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Server extends WebSocketServer {
+
+    ArrayList<WebSocket> websockets = new ArrayList<WebSocket>();
+    ArrayList<WebSocket> players = new ArrayList<WebSocket>();
+    HashMap<String, WebSocket> map = new HashMap<String, WebSocket>();
+
+    public Server(int port){
+        super(new InetSocketAddress(port));
+    }
+
+    public Server(InetSocketAddress address) { super(address); }
 
     @Override
     public void onOpen(WebSocket webSocket, ClientHandshake clientHandshake) {
@@ -29,5 +43,15 @@ public class Server extends WebSocketServer {
     @Override
     public void onStart() {
 
+    }
+
+    public static void main(String[] args) {
+        try {
+            Server server = new Server(12000);
+            server.start();
+            System.out.println("Server started on port: " + server.getPort());
+        } catch (Exception error) {
+            error.printStackTrace();
+        }
     }
 }
